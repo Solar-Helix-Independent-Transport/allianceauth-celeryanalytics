d86e919c7da5d187c05b41c7b48a3a530b8b334f
Aaron Kable <aaronkable@gmail.com>
2019-11-03 06:57:47 +0000

    admin update

--- celeryanalytics/admin.py
+++ celeryanalytics/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import CeleryTaskCompleted, CeleryTaskFailed
 
-admin.site.register(CeleryTaskCompleted)
-admin.site.register(CeleryTaskFailed)
 
+class FailedAdmin(admin.ModelAdmin):
+    list_display=('task', 'time', 'excep')
+    date_hierarchy = 'time'
+    list_filter=('task',)
+
+class CompleteAdmin(admin.ModelAdmin):
+    list_display=('task', 'time')
+    date_hierarchy = 'time'
+    list_filter=('task',)
+
+admin.site.register(CeleryTaskCompleted, CompleteAdmin)
+admin.site.register(CeleryTaskFailed, FailedAdmin)
