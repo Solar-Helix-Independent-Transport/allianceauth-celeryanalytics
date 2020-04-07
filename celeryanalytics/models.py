41d92d5fe51c527ea1a3cf56666660fb9b3c3fc0
AaronKable <aaronkable@gmail.com>
2020-04-07 22:30:28 +0800

    add migrations

--- celeryanalytics/models.py
+++ celeryanalytics/models.py
@@ -2,19 +2,32 @@ from django.db import models
 
 
 class CeleryTaskCompleted(models.Model):
-    task = models.TextField()
+    task = models.CharField(max_length=500)
     result = models.TextField()
     time = models.DateTimeField()
 
     def __str__(self):
         return "%s completed at: %s" % (self.task, self.time.strftime("%Y-%m-%d %H:%M:%S"))
 
+    class Meta:
+        indexes = (
+            models.Index(fields=['task']),
+            models.Index(fields=['time'])
+        )
+
 
 class CeleryTaskFailed(models.Model):
-    task = models.TextField()
+    task = models.CharField(max_length=500)
     excep = models.TextField()
     trace = models.TextField()
     time = models.DateTimeField()
 
     def __str__(self):
         return "%s failed at: %s with error %s" % (self.task, self.time.strftime("%Y-%m-%d %H:%M:%S"), self.excep)
+
+    class Meta:
+        indexes = (
+            models.Index(fields=['task']),
+            models.Index(fields=['time'])
+        )
+
