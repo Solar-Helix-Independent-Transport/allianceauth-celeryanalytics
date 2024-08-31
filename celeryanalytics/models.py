a682c58bb6de117b255996e4b18fdfe77aa06750
Joel Falknau <joel.falknau@gmail.com>
2024-09-01 02:19:19 +1000

    clarify task python function name, not celery registered name

--- celeryanalytics/models.py
+++ celeryanalytics/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class CeleryTaskCompleted(models.Model):
-    task = models.CharField(max_length=500)
+    task = models.CharField(max_length=500, help_text="This is the Full path to the python function module.name. Not the Celery Registered Task name")
     result = models.TextField()
     time = models.DateTimeField()
     runtime = models.FloatField(default=0)
+
     def __str__(self):
-        return "%s completed at: %s" % (self.task, self.time.strftime("%Y-%m-%d %H:%M:%S"))
+        return "{} completed at: {}".format(self.task, self.time.strftime("%Y-%m-%d %H:%M:%S"))
 
     class Meta:
         indexes = (
@@ -17,18 +18,17 @@ class CeleryTaskCompleted(models.Model):
 
 
 class CeleryTaskFailed(models.Model):
-    task = models.CharField(max_length=500)
+    task = models.CharField(max_length=500, help_text="This is the Full path to the python function module.name. Not the Celery Registered Task name")
     excep = models.TextField()
     trace = models.TextField()
     time = models.DateTimeField()
     runtime = models.FloatField(default=0)
 
     def __str__(self):
-        return "%s failed at: %s with error %s" % (self.task, self.time.strftime("%Y-%m-%d %H:%M:%S"), self.excep)
+        return "{} failed at: {} with error {}".format(self.task, self.time.strftime("%Y-%m-%d %H:%M:%S"), self.excep)
 
     class Meta:
         indexes = (
             models.Index(fields=['task']),
             models.Index(fields=['time'])
         )
-
