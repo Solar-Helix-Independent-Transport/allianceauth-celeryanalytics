3306085a6be2ec050f5198c65cb8bb20fe67e9f5
Matthew Moore <matthewemmoore@googlemail.com>
2026-01-22 12:40:05 +0000

    add asset-manifest.json and update copy-assets script (#13)

--- frontend/copy-assets.sh
+++ frontend/copy-assets.sh
@@ -2,8 +2,8 @@
 
 echo "Cleaning old assets."
 rm -rf ../celeryanalytics/static/celeryanalytics/static
-rm ../celeryanalytics/static/celeryanalytics/manifest.json
+rm ../celeryanalytics/static/celeryanalytics/asset-manifest.json
 echo "Copying new assets."
-cp build/static/ ../celeryanalytics/static/celeryanalytics/manifest.json
+cp build/asset-manifest.json ../celeryanalytics/static/celeryanalytics/asset-manifest.json
 cp -r build/static/ ../celeryanalytics/static/celeryanalytics/static
 echo "Assets copied successfully."
